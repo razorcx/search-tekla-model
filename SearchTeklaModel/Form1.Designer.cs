c16caf3fb5417da3ec21aac3d314a460cbae79a8
Cory LePage <cory@razorcx.com>
2017-12-10 10:14:49 -0800

    Initial commit

--- SearchTeklaModel/Form1.Designer.cs
+++ SearchTeklaModel/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SearchTeklaModel
 {
-	partial class Form1
+	partial class Search
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,12 +28,60 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.buttonSearch = new System.Windows.Forms.Button();
+			this.textBoxSearch = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::SearchTeklaModel.Properties.Resources.Logo;
+			this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(257, 67);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// buttonSearch
+			// 
+			this.buttonSearch.Location = new System.Drawing.Point(95, 137);
+			this.buttonSearch.Name = "buttonSearch";
+			this.buttonSearch.Size = new System.Drawing.Size(100, 36);
+			this.buttonSearch.TabIndex = 1;
+			this.buttonSearch.Text = "Search";
+			this.buttonSearch.UseVisualStyleBackColor = true;
+			this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+			// 
+			// textBoxSearch
+			// 
+			this.textBoxSearch.Location = new System.Drawing.Point(95, 109);
+			this.textBoxSearch.Name = "textBoxSearch";
+			this.textBoxSearch.Size = new System.Drawing.Size(100, 22);
+			this.textBoxSearch.TabIndex = 2;
+			// 
+			// Search
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(282, 206);
+			this.Controls.Add(this.textBoxSearch);
+			this.Controls.Add(this.buttonSearch);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Search";
+			this.Text = "Search";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button buttonSearch;
+		private System.Windows.Forms.TextBox textBoxSearch;
 	}
 }
 
--- SearchTeklaModel/Form1.cs
+++ SearchTeklaModel/Form1.cs
@@ -1,20 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
 
 namespace SearchTeklaModel
 {
-	public partial class Form1 : Form
+	public partial class Search : Form
 	{
-		public Form1()
+		public Search()
 		{
 			InitializeComponent();
+		}
+
+		private void buttonSearch_Click(object sender, EventArgs e)
+		{
+			//searching for member part mark
+			var searchValue = textBoxSearch.Text;
+
+			ModelObjectEnumerator.AutoFetch = true;
+			var modelObjects = new Model().GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.BEAM)
+				.ToList()
+				.OfType<Beam>()
+				.ToList();
+
+			var beams = modelObjects
+				.Where(b => b.GetPartMark()
+				.Contains(searchValue))
+				.ToList();
+
+			beams.ForEach(b =>
+			{
+				var drawer = new GraphicsDrawer();
+				var color = new Tekla.Structures.Model.UI.Color(1,1,1);
+				var location = b.GetCoordinateSystem().Origin;
+				drawer.DrawText(location, b.GetPartMark(), color);
+			});
+		}
+	}
+
+	public static class TeklaModelExtensions
+	{
+		public static List<ModelObject> ToList(this ModelObjectEnumerator enumerator)
+		{
+			var modelObjects = new List<ModelObject>();
+			while (enumerator.MoveNext())
+			{
+				var modelObject = enumerator.Current;
+				modelObjects.Add(modelObject);
+			}
+			return modelObjects;
 		}
 	}
 }
